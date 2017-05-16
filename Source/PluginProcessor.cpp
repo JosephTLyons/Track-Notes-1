e1f2fbf63842be958bcce07b219840bf8ceca4f0
Joseph Lyons <josephlyons@Josephs-MBP-15.PK5001Z>
2017-05-16 08:56:27 -0400

    Commented entire processor function out

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -123,26 +123,26 @@ bool TrackNotesAudioProcessor::isBusesLayoutSupported (const BusesLayout& layout
 
 void TrackNotesAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
 {
-    const int totalNumInputChannels  = getTotalNumInputChannels();
-    const int totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        float* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+//    const int totalNumInputChannels  = getTotalNumInputChannels();
+//    const int totalNumOutputChannels = getTotalNumOutputChannels();
+//
+//    // In case we have more outputs than inputs, this code clears any output
+//    // channels that didn't contain input data, (because these aren't
+//    // guaranteed to be empty - they may contain garbage).
+//    // This is here to avoid people getting screaming feedback
+//    // when they first compile a plugin, but obviously you don't need to keep
+//    // this code if your algorithm always overwrites all the output channels.
+//    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+//        buffer.clear (i, 0, buffer.getNumSamples());
+//
+//    // This is the place where you'd normally do the guts of your plugin's
+//    // audio processing...
+//    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+//    {
+//        float* channelData = buffer.getWritePointer (channel);
+//
+//        // ..do something to the data...
+//    }
 }
 
 //==============================================================================
@@ -202,10 +202,10 @@ void TrackNotesAudioProcessor::setStateInformation (const void* data, int sizeIn
         {
             // ok, now pull our strings
             
-//            *performersNameEditorPtr->setText(xmlState->getStringAttribute("performersName", *performersNameEditorPtr-getT);)
-//            *instrumentPlayedEditorPtr = xmlState->getStringAttribute("instrumentPlayed", *instrumentPlayedEditorPtr);
-//            *microphonesUsedString = xmlState->getStringAttribute("microphonesUsed", *microphonesUsedString);
-//            *generalNotesString = xmlState->getStringAttribute("generalNotes", *generalNotesString);
+            performersNameEditorPtr->setText(xmlState->getStringAttribute("performersName"));
+            instrumentPlayedEditorPtr->setText(xmlState->getStringAttribute("instrumentPlayed"));
+            microphonesUsedEditorPtr->setText(xmlState->getStringAttribute("microphonesUsed"));
+            generalNotesEditorPtr->setText(xmlState->getStringAttribute("generalNotes"));
             
             // Now reload our parameters..
             for (int i = 0; i < getNumParameters(); ++i)
