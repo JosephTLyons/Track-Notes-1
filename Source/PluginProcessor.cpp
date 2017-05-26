f2f0565e09d0824e4a163e7603619f3880ffc94d
Joseph Lyons <josephlyons@Josephs-MacBook-Pro-15.local>
2017-05-26 07:09:32 -0400

    Added code back from Demo Plugin

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -167,6 +167,12 @@ void TrackNotesAudioProcessor::getStateInformation (MemoryBlock& destData)
         xml.setAttribute ("imageTwo", imageData.toBase64Encoding());
     }
     
+    
+    // Store the values of all our parameters, using their param ID as the XML attribute
+    for (int i = 0; i < getNumParameters(); ++i)
+        if (AudioProcessorParameterWithID* p = dynamic_cast<AudioProcessorParameterWithID*> (getParameters().getUnchecked(i)))
+            xml.setAttribute (p->paramID, p->getValue());
+    
     // Use this helper function to stuff it into the binary blob and return it..
     copyXmlToBinary (xml, destData);
 }
@@ -207,6 +213,11 @@ void TrackNotesAudioProcessor::setStateInformation (const void* data, int sizeIn
             
             imageTwo = ImageFileFormat::loadFrom(*memoryInput);
             delete memoryInput;
+            
+            // Now reload our parameters..
+            for (int i = 0; i < getNumParameters(); ++i)
+                if (AudioProcessorParameterWithID* p = dynamic_cast<AudioProcessorParameterWithID*> (getParameters().getUnchecked(i)))
+                    p->setValue ((float) xml->getDoubleAttribute (p->paramID, p->getValue()));
         }
     }
 }
