331e12e3ad45716fef2ad614f725f361ffefefd1
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-07-24 18:51:27 -0400

    Added header licenses to all my code files

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,9 +1,21 @@
 /*
   ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin processor.
+  Copyright 2013-2017 Joseph Lyons
+  
+  This file is part of Track Notes.
+ 
+  Track Notes is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  Track Notes is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with Track Notes.  If not, see <http://www.gnu.org/licenses/>.
 
   ==============================================================================
 */
--- Source/StaticTextSizeButton.hpp
+++ Source/StaticTextSizeButton.hpp
@@ -1,10 +1,31 @@
-//
-//  StaticTextSizeButton.hpp
-//  Track Notes
-//
-//  Created by Joseph Lyons on 6/20/17.
-//
-//
+/*
+  ==============================================================================
+  Copyright 2013-2017 Joseph Lyons
+ 
+  This file is part of Track Notes.
+ 
+  Track Notes is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  Track Notes is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with Track Notes.  If not, see <http://www.gnu.org/licenses/>.
+ 
+  Created with Projucer version: 5.0.2
+ 
+  ------------------------------------------------------------------------------
+ 
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+ 
+  ==============================================================================
+*/
 
 #ifndef StaticTextSizeButton_hpp
 #define StaticTextSizeButton_hpp
